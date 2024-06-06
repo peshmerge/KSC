3e6a3e2f049923d9682e925acd426d5fc62c5927
Peshmerge <pesho.morad@gmail.com>
2024-06-06 23:24:24 +0200

    Added a floating text 'Copied' when the user click on the transliterated text to let tthe user know that the text has been copied to the clipboard. In addition, I provided explanation on why I made some specific choices/hacks/work-arounds.

--- content.js
+++ content.js
@@ -1,17 +1,39 @@
+/*
+Part of an somehow an ugly work-around to remove the popup when the user 
+outside the popup. 
+*/
+
 var popupCount = 0;
 
+// Initialize the count.
 window.onload = function () {
   popupCount = 0;
 };
+/*
+For both scripts, I included only the letters that make them distinguishable 
+from Arabic and Persian in the case of Central Kurdish, and from other 
+latin-based scripts in the case of Northern Kurdish. 
+This is again a work-around to avoid displaying the popup on websites where non
+Kurdish texts are being highlighted. 
+
+A better solution would be checking the html language code reference attribute
+ <html lang=""> which in case of Kurdish could be either
+ <html lang="ku"> Kurdish as general (ISO 639-1)
+ <html lang="kmr"> Northern kurdish (ISO 639-3)
+ <html lang="ckb"> Central Kurdish (ISO 639-3) 
+
+ However, not all Kurdish websites are using this attribute correctly.
+*/
 const kurdishPersianArabicAlphabet = [
-  'ا', 'ب', 'پ', 'ت', 'ج', 'چ', 'ح', 'خ', 'د', 'ر', 'ز', 'ژ', 'س', 'ش', 'ع', 'غ', 'ف', 'ق', 'ک', 'گ', 'ل', 'م', 'ن', 'ه', 'و', 'ی', 'ێ', 'ە'
+  'ا', 'پ', 'چ', 'ژ', 'ک', 'گ', 'ل', 'ێ', 'ە', 'ڵ', 'ئـ', 'وو', 'ڕ', '	ۆ'
 ];
 
 const kurdishLatinAlphabet = [
-  'A', 'B', 'C', 'Ç', 'D', 'E', 'Ê', 'F', 'G', 'H', 'I', 'Î', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'Ş', 'T', 'U', 'Û', 'V', 'W', 'X', 'Y', 'Z',
-  'a', 'b', 'c', 'ç', 'd', 'e', 'ê', 'f', 'g', 'h', 'i', 'î', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 'ş', 't', 'u', 'û', 'v', 'w', 'x', 'y', 'z'
+  'Ç', 'Ê', 'Î', 'Ş', 'Û',
+  'ç', 'ê', 'î', 'ş', 'û',
 ];
 
+// Limit the rate of function execution
 function debounce(fn, delay) {
   let timer = null;
   return function () {
@@ -23,18 +45,57 @@ function debounce(fn, delay) {
   };
 };
 
+function showFloatingText(timeout) {
+  removeElementByID('floating-text');
+  var floatingDiv = document.createElement('div');
+  floatingDiv.id = "floatingText";
+  floatingDiv.className = "floatingText";
+  floatingDiv.textContent = "Copied!";
+
+  // Apply CSS using J
+  floatingDiv.style.direction = "ltr";
+  floatingDiv.style.position = 'fixed';
+  floatingDiv.style.top = '50%';
+  floatingDiv.style.left = '50%';
+  floatingDiv.style.transform = 'translate(-50%, -50%)';
+  floatingDiv.style.backgroundColor = 'rgba(255, 187, 0, 0.8)';
+  floatingDiv.style.color = 'white';
+  floatingDiv.style.padding = '10px';
+  floatingDiv.style.borderRadius = '5px';
+  floatingDiv.style.fontSize = '24px';
+  floatingDiv.style.zIndex = '100000';
+  floatingDiv.style.opacity = '0';
+  floatingDiv.style.display = 'none';
+  floatingDiv.style.transition = 'opacity 1s ease-in-out';
+  document.body.appendChild(floatingDiv);
+  floatingDiv.style.display = 'block';
+  void floatingDiv.offsetWidth;
+  floatingDiv.style.opacity = '1';
+  setTimeout(
+    () => {
+      floatingDiv.style.opacity = '0';
+      setTimeout(() => {
+        removeElementByID('floating-text');
+      }, 1000);
+    }, timeout
+  );
+}
+// Show the popup and transliterate the highlighted text on selectionChange event.
 document.addEventListener("selectionchange", debounce(function (event) {
-  let highlighted_text = document.getSelection ? document.getSelection().toString() : document.selection.createRange().toString();
-  if (highlighted_text.length > 0) {
+  let selection = document.getSelection ? document.getSelection().toString() :
+    document.selection.createRange().toString();
+  if (selection.length > 0) {
+    // addFloatingTextDiv();
     let rect = window.getSelection().getRangeAt(0).getBoundingClientRect();
     popupCount = 1;
-    showPopup(rect, highlighted_text);
+    showPopup(rect, selection);
   }
-}, 50));
+}, 10));
 
+// Ugly work-around (hack) to remove the popup when the user click outside. 
 document.addEventListener('click', function (event) {
   if (popupCount == 0) {
-    remove_popup();
+    removeElementByID('transliteration-popup');
   } else {
     popupCount = 0;
   }
@@ -43,15 +104,14 @@ document.addEventListener('click', function (event) {
 
 function showPopup(rect, highlighted_text) {
   //Remove any other before adding a new one.
-  remove_popup();
+  removeElementByID('transliteration-popup');
 
   // Declare and create popup
   let popup_element = document.createElement('div');
-  popup_element.id = 'lookupPopup';
+  popup_element.id = 'transliteration-popup';
 
   // Check what script are we dealing with, Central Kurdish or Northern Kurdish
   let script_type = detectScript(highlighted_text);
-
   // Based on the detected script, show the options related to the specific script. 
   if (script_type === "PAS") {
     popup_element.style.direction = "ltr";
@@ -61,9 +121,15 @@ function showPopup(rect, highlighted_text) {
     popup_element.style.direction = "rtl";
     popup_element.style.textAlign = "right";
     popup_element.appendChild(addLASRadioOptions(highlighted_text, script_type));
+  } else {
+    return;
   }
   // Convert the highlighted text based on the detected script
-  transliterated_text = transliterate(highlighted_text, script_type, "diacritical");
+  transliterated_text = transliterate(
+    highlighted_text,
+    script_type,
+    "diacritical"
+  );
 
   // Add a div to contain the transliterated text 
   let transliteration_text_div = document.createElement('div');
@@ -81,17 +147,19 @@ function showPopup(rect, highlighted_text) {
   transliteration_text_div.appendChild(tratransliterated_text_element);
 
 
-  // Append the transliteration_text_div element to the container div lookupPopup
+  // Append to the container div transliterationPopup
   popup_element.appendChild(transliteration_text_div);
 
-  // Change position of the lookupPopup
+  // Change position of the transliterationPopup
   popup_element.style.left = `${rect.left + window.scrollX}px`;
-  popup_element.style.top = `${rect.top + window.scrollY + rect.height +10}px`;
+  popup_element.style.top = `${rect.top + window.scrollY + rect.height + 10}px`;
 
   popup_element.addEventListener("click", function (event) {
     popupCount = 1;
-    navigator.clipboard.writeText(tratransliterated_text_element.innerText).then(function () {
-      console.log('Text copied to clipboard');
+    navigator.clipboard.writeText(
+      tratransliterated_text_element.innerText
+    ).then(function () {
+      showFloatingText(2000);
     }).catch(function (error) {
       console.error('Failed to copy seelcted text: ', error);
     });
@@ -103,11 +171,14 @@ function showPopup(rect, highlighted_text) {
     event.preventDefault();
   });
 
-  // Append the lookupPopup to the page.
+  // Append the transliterationPopup to the page.
   document.body.appendChild(popup_element);
 }
 
-// Shows the options related to converting script from Central Kurdish (Persian-Arabic) to Northern Kurdish (Hawar/Latin) script.
+/*
+Shows the options related to converting script from 
+Central Kurdish (Persian-Arabic) to Northern Kurdish (Hawar/Latin) script.
+*/
 function addPASRadioOptions(highlighted_text, script_type) {
   const container = document.createElement('div');
   container.id = "PAS-options-container";
@@ -128,7 +199,9 @@ function addPASRadioOptions(highlighted_text, script_type) {
     }
     radioButton.addEventListener('click', function () {
       transliterated_text = transliterate(highlighted_text, script_type, option);
-      (document.getElementById("transliterated_element")).innerHTML = transliterated_text;
+      (
+        document.getElementById("transliterated_element")
+      ).innerHTML = transliterated_text;
     });
 
     // Create a label for the checkbox
@@ -141,7 +214,10 @@ function addPASRadioOptions(highlighted_text, script_type) {
   return container;
 }
 
-// Shows the options related to converting script from Northern Kurdish (Hawar/Latin) to Central Kurdish (Persian-Arabic) script.
+/*
+Shows the options related to converting script from 
+Northern Kurdish (Hawar/Latin) to Central Kurdish (Persian-Arabic) script.
+*/
 function addLASRadioOptions(highlighted_text, script_type) {
   const container = document.createElement('div');
   container.id = "LAS-options-container";
@@ -161,8 +237,12 @@ function addLASRadioOptions(highlighted_text, script_type) {
       radioButton.checked = true;
     }
     radioButton.addEventListener('click', function () {
-      transliterated_text = transliterate(highlighted_text, script_type, option);
-      (document.getElementById("transliterated_element")).innerHTML = transliterated_text;
+      transliterated_text = transliterate(
+        highlighted_text, script_type, option);
+      (
+        document.getElementById(
+          "transliterated_element"
+        )).innerHTML = transliterated_text;
     });
 
     // Create a label for the checkbox
@@ -175,15 +255,16 @@ function addLASRadioOptions(highlighted_text, script_type) {
   return container;
 }
 
-function remove_popup() {
-  popup = document.getElementById('lookupPopup');
-  if (popup) {
-    popup.remove();
+function removeElementByID(elemetnID) {
+  elementToRemove = document.getElementById(elemetnID);
+  if (elementToRemove) {
+    elementToRemove.remove();
   }
 }
 
-//  We use this function to determine the script. It's not always accurate, but it's a rough estimation since not all Kurdish websites use the correct 
-//   language code (ISO639-3) tag in html <html lang="">. ckb for Central Kurdish or kmr for Northern Kurdish.
+/* 
+We use this function to determine the script. See explanation above! 
+*/
 function detectScript(highlighted_text) {
   const persianArabicSet = new Set(kurdishPersianArabicAlphabet);
   const latinSet = new Set(kurdishLatinAlphabet);
@@ -198,11 +279,19 @@ function detectScript(highlighted_text) {
       latinCounter = latinCounter + 1;
     }
   }
-  // PAS : Persian-Arabic-based script (Central Kurdish or Northern Kurdish written in this scrip
-  // LAS : Latin-based script aka Hawar script (therefore Northern Kurdish) 
-  return persianArabicCounter > latinCounter ? "PAS" : "LAS";
+  if (persianArabicCounter > latinCounter) {
+    // PAS : Persian-Arabic-based script (Central Kurdish) 
+    // or Northern Kurdish written in this script.
+
+    return "PAS";
+  } else if (persianArabicCounter < latinCounter) {
+    // LAS : Latin-based script aka Hawar script (therefore Northern Kurdish) 
+    return "LAS";
+  }
+  return;
 }
 
+// The actual transliterate function that uses functions from Kurdinus.js
 function transliterate(highlighted_text, script_type, option = "diacritical") {
   if (script_type === "PAS") {
     // Persian-Arabic to Latin
